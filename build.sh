462f50f0f0bc3e726d9ed8848b8b9660a3e0a7e8
Sam Stern <samstern@google.com>
2020-02-18 14:52:44 -0800

    Build script

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+
+# Copy mock google-services file
+echo "Using mock google-services.json"
+cp mock-google-services.json build-android/app/google-services.json
+cp mock-google-services.json build-android-start/app/google-services.json
+cp mock-google-services.json grow-android-start/app/google-services.json
+
+# Build
+./gradlew clean build
