cf0614203a2693b8168c3c0b89c9736206a456ee
Rachel Collins <racheldosh@google.com>
2021-04-07 16:34:24 -0700

    FriendlyEats Kotlin app build-android-start shell

---
+++ build-android-start/app/src/main/java/com/google/firebase/codelab/friendlychat/MyScrollToBottomObserver.kt
@@ -0,0 +1,40 @@
+/**
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.firebase.codelab.friendlychat
+
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.AdapterDataObserver
+
+class MyScrollToBottomObserver(
+    private val recycler: RecyclerView,
+    private val adapter: FriendlyMessageAdapter,
+    private val manager: LinearLayoutManager
+) : AdapterDataObserver() {
+    override fun onItemRangeInserted(positionStart: Int, itemCount: Int) {
+        super.onItemRangeInserted(positionStart, itemCount)
+        val count = adapter.itemCount
+        val lastVisiblePosition = manager.findLastCompletelyVisibleItemPosition()
+        // If the recycler view is initially being loaded or the
+        // user is at the bottom of the list, scroll to the bottom
+        // of the list to show the newly added message.
+        val loading = lastVisiblePosition == -1
+        val atBottom = positionStart >= count - 1 && lastVisiblePosition == positionStart - 1
+        if (loading || atBottom) {
+            recycler.scrollToPosition(positionStart)
+        }
+    }
+}
