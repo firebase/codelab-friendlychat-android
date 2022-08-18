8ed81a9d66eb6e056c0a4ab1691f3aab13d713db
rosariopf <rosariopf@google.com>
2022-08-18 16:04:21 +0100

    make FriendlyMessage model a data class

--- build-android-start/app/src/main/java/com/google/firebase/codelab/friendlychat/model/FriendlyMessage.kt
+++ build-android-start/app/src/main/java/com/google/firebase/codelab/friendlychat/model/FriendlyMessage.kt
@@ -15,19 +15,9 @@
  */
 package com.google.firebase.codelab.friendlychat.model
 
-class FriendlyMessage {
-    var text: String? = null
-    var name: String? = null
-    var photoUrl: String? = null
-    var imageUrl: String? = null
-
-    // Empty constructor needed for Firestore serialization
-    constructor()
-
-    constructor(text: String?, name: String?, photoUrl: String?, imageUrl: String?) {
-        this.text = text
-        this.name = name
-        this.photoUrl = photoUrl
-        this.imageUrl = imageUrl
-    }
-}
+data class FriendlyMessage(
+    val text: String? = null,
+    val name: String? = null,
+    val photoUrl: String? = null,
+    val imageUrl: String? = null,
+)
