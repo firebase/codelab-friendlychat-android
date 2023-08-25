69cff57222560a2266fc38c3f3b32719f0faf710
DPEBot <dpebot@google.com>
2023-08-25 10:16:17 -0700

    Auto-update dependencies. (#267)

--- build-android-start/app/build.gradle.kts
+++ build-android-start/app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.google.firebase.codelab.friendlychat"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.google.firebase.codelab.friendlychat"
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
