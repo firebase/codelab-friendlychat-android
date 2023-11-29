3aff5c577cbfbadf7e8c9aa9d93d983257873630
DPEBot <dpebot@google.com>
2023-11-29 11:04:28 -0800

    Auto-update dependencies. (#281)

--- build-android/app/build.gradle.kts
+++ build-android/app/build.gradle.kts
@@ -52,7 +52,7 @@ dependencies {
     implementation("com.github.bumptech.glide:glide:4.16.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("androidx.media:media:1.6.0")
+    implementation("androidx.media:media:1.7.0")
     implementation("androidx.core:core-ktx:1.12.0")
 
     // Google
