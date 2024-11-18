f108d847efad8c8d15363dd2c3caa92d4038c919
DPEBot <dpebot@google.com>
2024-11-18 09:24:09 -0500

    Auto-update dependencies. (#315)

--- build-android-start/app/build.gradle.kts
+++ build-android-start/app/build.gradle.kts
@@ -59,7 +59,7 @@ dependencies {
     implementation("com.google.android.gms:play-services-auth:21.2.0")
 
     // Firebase
-    implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
+    implementation(platform("com.google.firebase:firebase-bom:33.6.0"))
     implementation("com.google.firebase:firebase-database")
     implementation("com.google.firebase:firebase-storage")
     implementation("com.google.firebase:firebase-auth")
