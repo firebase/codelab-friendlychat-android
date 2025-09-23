caeed75328fba2bcca5f8fab0e68780a4467db56
tsvetkovamp <mariana_dincheva@epam.com>
2025-09-23 14:08:59 +0300

    Test commit

--- build-android-start/app/src/main/java/com/google/firebase/codelab/friendlychat/MyButtonObserver.kt
+++ build-android-start/app/src/main/java/com/google/firebase/codelab/friendlychat/MyButtonObserver.kt
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/** testing */
 package com.google.firebase.codelab.friendlychat
 
 import android.text.Editable
