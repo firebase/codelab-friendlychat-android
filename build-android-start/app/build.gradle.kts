5c21196c3b6060b9206e82070b186881acc2bdd8
Rosario Fernandes <rosariopf@google.com>
2026-01-27 22:12:21 +0000

    chore: update build.gradle.kts files

--- build-android-start/app/build.gradle.kts
+++ build-android-start/app/build.gradle.kts
@@ -2,7 +2,6 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     id("com.android.application")
-    id("kotlin-android")
     id("com.google.gms.google-services")
 }
 
@@ -27,7 +26,7 @@ android {
     buildTypes {
         getByName("release") {
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
 
