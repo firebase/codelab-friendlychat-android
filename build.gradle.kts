acb8673a2c96c80dc6ac0d9e53a0b05f7b72e4c3
Rosário P. Fernandes <rosariopf@google.com>
2026-04-30 04:15:22 +0100

    chore(*): update gradle-versions plugin to 0.54.0 (#364)

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     id("com.android.library") version "9.0.0" apply false
     id("org.jetbrains.kotlin.android") version "2.3.0" apply false
     id("com.google.gms.google-services") version "4.4.4" apply false
-    id("com.github.ben-manes.versions") version "0.53.0" apply true
+    id("com.github.ben-manes.versions") version "0.54.0" apply true
 }
 
 allprojects {
