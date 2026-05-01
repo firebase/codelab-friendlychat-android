52401e1a9d391ac57fb5bf75e9931956cb8ed29e
DPEBot <dpebot@google.com>
2026-05-01 11:20:25 -0400

    Auto-update dependencies. (#365)
    * Auto-update dependencies.
    
    * chore(*): update Gradle 9.3.0 --> 9.5.0
    
    ---------
    
    Co-authored-by: Rosário P. Fernandes <rosariofernandes51@gmail.com>

--- build-android-start/app/build.gradle.kts
+++ build-android-start/app/build.gradle.kts
@@ -53,17 +53,17 @@ android {
 
 dependencies {
     implementation("com.google.android.material:material:1.13.0")
-    implementation("com.github.bumptech.glide:glide:5.0.5")
+    implementation("com.github.bumptech.glide:glide:5.0.7")
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("androidx.media:media:1.7.1")
-    implementation("androidx.core:core-ktx:1.17.0")
+    implementation("androidx.core:core-ktx:1.18.0")
 
     // Google
-    implementation("com.google.android.gms:play-services-auth:21.5.0")
+    implementation("com.google.android.gms:play-services-auth:21.5.1")
 
     // Firebase
-    implementation(platform("com.google.firebase:firebase-bom:34.8.0"))
+    implementation(platform("com.google.firebase:firebase-bom:34.12.0"))
     implementation("com.google.firebase:firebase-database")
     implementation("com.google.firebase:firebase-storage")
     implementation("com.google.firebase:firebase-auth")
